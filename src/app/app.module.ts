3345d62945d7de609ca27701402e33866b7d1389
selvendiranj <selvendiranj@gmail.com>
2017-11-30 16:39:25 +0530

    best practices changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,38 +41,40 @@ export class AppModule
     {
         router.events
             .filter((event: any) => event instanceof NavigationEnd)
-            .subscribe((event: NavigationEnd) =>
-            {
-                // We only receive NavigationEnd events
-                switch (event.url)
+            .subscribe(this.OnNavigate);
+    }
+
+    private OnNavigate(event: NavigationEnd): any
+    {
+        // We only receive NavigationEnd events
+        switch (event.url)
+        {
+            case '/layouts/boxed':
+            case '/layouts/horzboxed':
+                $('#container').removeClass('effect mainnav-lg navbar-fixed mainnav-fixed');
+                $('#container').addClass('effect mainnav-lg boxed-layout');
+                $('#navbar').addClass('navbar-boxed');
+                if (event.url === '/layouts/horzboxed')
                 {
-                    case '/layouts/boxed':
-                    case '/layouts/horzboxed':
-                        $('#container').removeClass('effect mainnav-lg navbar-fixed mainnav-fixed');
-                        $('#container').addClass('effect mainnav-lg boxed-layout');
-                        $('#navbar').addClass('navbar-boxed');
-                        if(event.url === '/layouts/horzboxed')
-                        {
-                            this.router.navigate(['/layouts/horzmenu']);
-                        }
-                        break;
-                    case '/layouts/blank':
-                        $('#container').removeClass('effect mainnav-lg boxed-layout');
-                        $('#navbar').removeClass('navbar-boxed');
-                        $('#container').addClass('effect mainnav-lg navbar-fixed mainnav-fixed');
-                        break;
-                    case '/layouts/pushmenu':
-                        $.senseuiNav('pushToggle');
-                        break;
-                    case '/layouts/slidemenu':
-                        $.senseuiNav('slideToggle');
-                        break;
-                    case '/layouts/sidebar':
-                        $.senseuiNav('colExpToggle');
-                        break;
-                    default:
-                        break;
+                    this.router.navigate(['/layouts/horzmenu']);
                 }
-            });
+                break;
+            case '/layouts/blank':
+                $('#container').removeClass('effect mainnav-lg boxed-layout');
+                $('#navbar').removeClass('navbar-boxed');
+                $('#container').addClass('effect mainnav-lg navbar-fixed mainnav-fixed');
+                break;
+            case '/layouts/pushmenu':
+                $.senseuiNav('pushToggle');
+                break;
+            case '/layouts/slidemenu':
+                $.senseuiNav('slideToggle');
+                break;
+            case '/layouts/sidebar':
+                $.senseuiNav('colExpToggle');
+                break;
+            default:
+                break;
+        }
     }
 }
